837dc69c2586cb9f5d667569fa415c545ef2841b
Idowu Micheal <idowumicheal2015@gmail.com>
2023-11-18 11:19:07 +0100

    commit

--- webcar/settings.py
+++ webcar/settings.py
@@ -96,20 +96,20 @@ WSGI_APPLICATION = 'webcar.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-if not DEBUG:
+# if not DEBUG:
 
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
+#     }
 
-#Postgresql Database
-else:
-    DATABASES = {
-        'default': dj_database_url.parse(env('DATABASE_URL'))
-    }
+# #Postgresql Database
+# else:
+DATABASES = {
+    'default': dj_database_url.parse(env('DATABASE_URL'))
+}
 
 
 
