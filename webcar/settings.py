80afbd8f83b0efb410c37715b85bd78ae444748e
Idowu Micheal <idowumicheal2015@gmail.com>
2023-11-19 03:22:42 +0100

    commit

--- webcar/settings.py
+++ webcar/settings.py
@@ -81,7 +81,7 @@ ROOT_URLCONF = 'webcar.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR/'templates'],
+        'DIRS': os.path.join(BASE_DIR,'templates'),
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -160,14 +160,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS=[
-     BASE_DIR/'static'
- ]
-STATIC_ROOT=BASE_DIR/'staticfiles'
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
+# STATICFILES_DIRS=[
+#     BASE_DIR/'static'
+#  ]
 STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 #Media Files
-MEDIA_ROOT=BASE_DIR/'media'
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 MEDIA_URL='/media/'
 
 # Default primary key field type
