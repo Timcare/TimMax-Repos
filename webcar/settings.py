20a4e2b2b232319a5baed1ef60cabcd8f71788de
Idowu Micheal <107912847+Timcare@users.noreply.github.com>
2023-11-19 19:40:03 +0100

    Update settings.py

--- webcar/settings.py
+++ webcar/settings.py
@@ -99,18 +99,10 @@ WSGI_APPLICATION = 'webcar.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-if not DEBUG:
     # #Postgresql Database
  DATABASES = {
      'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
  }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-            }
-    }
 
 
 
