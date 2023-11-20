b56b263360ff6869cd844a57c1d816b4ae0aa9fa
Idowu Micheal <107912847+Timcare@users.noreply.github.com>
2023-11-20 06:25:49 +0100

    Update settings.py

--- webcar/settings.py
+++ webcar/settings.py
@@ -160,11 +160,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT=os.path.join(BASE_DIR,'static')
+if not DEBUG
+    STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 # STATICFILES_DIRS=[
 #     BASE_DIR/'static'
 #  ]
-STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
+    STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
 #Media Files
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 MEDIA_URL='/media/'
